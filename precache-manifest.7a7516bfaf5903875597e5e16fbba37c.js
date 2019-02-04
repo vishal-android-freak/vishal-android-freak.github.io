9c6f38dadea29dcea6e0ead285c8af9d3ae61b79
Vishal Dubey <yzvishal.vd@gmail.com>
2019-02-04 12:55:16 +0530

    initial revamp

---
+++ precache-manifest.7a7516bfaf5903875597e5e16fbba37c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "03807947979bf420e3cd",
+    "url": "/static/js/main.03807947.chunk.js"
+  },
+  {
+    "revision": "030fb6a4ba75ac2d373b",
+    "url": "/static/js/1.030fb6a4.chunk.js"
+  },
+  {
+    "revision": "03807947979bf420e3cd",
+    "url": "/static/css/main.8c5c6842.chunk.css"
+  },
+  {
+    "revision": "7f8709a2887e5904536a1ca5ef8c65bd",
+    "url": "/index.html"
+  }
+];
